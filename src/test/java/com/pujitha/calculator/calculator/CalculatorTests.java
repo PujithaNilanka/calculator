7692440f1989097da609e5b676770ae46c52e20b
Pujitha Nilanka Pathirana <pujitha.nilanka@gmail.com>
2018-12-02 23:15:26 +0000

    Add missing java docs in test classes

--- src/test/java/com/pujitha/calculator/calculator/CalculatorTests.java
+++ src/test/java/com/pujitha/calculator/calculator/CalculatorTests.java
@@ -4,9 +4,15 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
+/**
+ * Test class
+ * */
 public class CalculatorTests {
     private Calculator calculator = new Calculator();
 
+    /**
+     * Test sum method
+     * */
     @Test
     public void testSum() {
         assertEquals(5, calculator.sum(2, 3));
