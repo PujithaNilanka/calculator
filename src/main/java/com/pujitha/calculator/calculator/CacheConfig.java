d2d4ffa4f6d6b99899135ba2330b7f2373dad7cf
pujitha.nilanka@gmail.com <Nilanka@1978Git>
2018-12-23 01:38:45 +0000

    Add redis cache configs

--- src/main/java/com/pujitha/calculator/calculator/CacheConfig.java
+++ src/main/java/com/pujitha/calculator/calculator/CacheConfig.java
@@ -1,4 +1,43 @@
 package com.pujitha.calculator.calculator;
 
-public class CacheConfig {
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+/**
+ * Cache configuration
+ */
+@Configuration
+@EnableCaching
+public class CacheConfig extends CachingConfigurerSupport{
+/*
+
+    @Bean
+    public CacheManager cacheManager(RedisTemplate redisTemplate) {
+        return new RedisCacheManager(redisTemplate(redisConnectionFactory()));
+    }
+    */
+
+    @Bean
+    JedisConnectionFactory jedisConnectionFactory() {
+        JedisConnectionFactory jedisConFactory
+                = new JedisConnectionFactory();
+        jedisConFactory.setHostName("redis");
+        jedisConFactory.setPort(6379);
+        return new JedisConnectionFactory();
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(jedisConnectionFactory());
+        return template;
+    }
+
 }
--- src/main/java/com/pujitha/calculator/calculator/Calculator.java
+++ src/main/java/com/pujitha/calculator/calculator/Calculator.java
@@ -1,12 +1,14 @@
 package com.pujitha.calculator.calculator;
 
 import org.springframework.stereotype.Service;
+import org.springframework.cache.annotation.Cacheable;
 
 /**
  * Service implementation
  */
 @Service
 public class Calculator {
+    @Cacheable
     int sum(int a, int b) {
         return a + b;
     }
